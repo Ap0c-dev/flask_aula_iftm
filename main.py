5d19ed0f69688305ec315e72e24102be7437f502
TIAGO DE ALCANTARA E SILVA BARROS <09142798663>
2022-11-01 09:22:17 -0300

    Criando a rota predict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -8,6 +8,12 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method== 'POST':
+        return jsonify(request.form)
+    elif request.method == 'GET':
+        return jsonify({"mansagem" : "utilize o formulário"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
